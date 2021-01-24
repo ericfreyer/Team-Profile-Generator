bd0603ca967c1cfd8f21116ef861fbb2678359c3
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-01-24 14:56:10 -0500

    figured out about constructor functions

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 const writeFileAsync = util.promisify(fs.writeFile);
+const cards = require("./user_cards");
+
+
+class Employee{
+    constructor()
+}
+class Manager{
+    constructor()
+}
+class Intern{
+    constructor()
+}
+class Engineer{
+    constructor()
+}
