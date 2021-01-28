42d9a548179e65d382ff02d667cd130267bea80a
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-01-28 18:47:16 -0500

    inquirer works

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const util = require("util");
 const inquirer = require("inquirer");
 const writeFileAsync = util.promisify(fs.writeFile);
 const cards = require("./user_cards");
-
+start()
 
 
 class Employee{
@@ -123,4 +123,127 @@ Inquirer()
     })
 }
 
-start()
+
+
+
+
+// create the team
+// const generateTeam = team => {
+//     ​
+//         // create the manager html
+//         const generateManager = manager => {
+//             return `
+//             <div class="card employee-card">
+//             <div class="card-header">
+//                 <h2 class="card-title">${manager.name()}</h2>
+//                 <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.position()}</h3>
+//             </div>
+//             <div class="card-body">
+//                 <ul class="list-group">
+//                     <li class="list-group-item">ID: ${manager.id()}</li>
+//                     <li class="list-group-item">Email: <a href="mailto:${manager.email()}}">${manager.email()}</a></li>
+//                     <li class="list-group-item">Office number: ${manager.officeNumber()}}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//             `;
+//         };
+//     ​
+//         // create the html for engineers
+//         const generateEngineer = engineer => {
+//             return `
+//             <div class="card employee-card">
+//         <div class="card-header">
+//             <h2 class="card-title">${engineer.name()}</h2>
+//             <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.position()}</h3>
+//         </div>
+//         <div class="card-body">
+//             <ul class="list-group">
+//                 <li class="list-group-item">ID: ${engineer.id()}</li>
+//                 <li class="list-group-item">Email: <a href="mailto:${engineer.email()}">${engineer.email()}</a></li>
+//                 <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github()}" target="_blank" rel="noopener noreferrer">${engineer.github()}</a></li>
+//             </ul>
+//         </div>
+//     </div>
+//             `;
+//         };
+//     ​
+//         // create the html for interns
+//         const generateIntern = intern => {
+//             return `
+//             <div class="card employee-card">
+//         <div class="card-header">
+//             <h2 class="card-title">${intern.name()}</h2>
+//             <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.position()}</h3>
+//         </div>
+//         <div class="card-body">
+//             <ul class="list-group">
+//                 <li class="list-group-item">ID: {{ id }}</li>
+//                 <li class="list-group-item">Email: <a href="mailto:${intern.email()}">${intern.email()}</a></li>
+//                 <li class="list-group-item">School: ${intern.school()}</li>
+//             </ul>
+//         </div>
+//     </div>
+//             `;
+//         };
+//     ​
+//         const html = [];
+//     ​
+//         html.push(team
+//             .filter(employee => employee.position() === "Manager")
+//             .map(manager => generateManager(manager))
+//         );
+//         html.push(team
+//             .filter(employee => employee.position() === "Engineer")
+//             .map(engineer => generateEngineer(engineer))
+//             .join("")
+//         );
+//         html.push(team
+//             .filter(employee => employee.position() === "Intern")
+//             .map(intern => generateIntern(intern))
+//             .join("")
+//         );
+//     ​
+//         return html.join("");
+//     ​
+//     }
+//     ​start()
+//     // export function to generate entire page
+//     module.exports = team => {
+//     ​
+//         return `
+//         <!DOCTYPE html>
+//     <html lang="en">
+//     ​
+//     <head>
+//         <meta charset="UTF-8" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+//         <title>My Team</title>
+//         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+//             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+//         <link rel="stylesheet" href="style.css">
+//         <script src="https://kit.fontawesome.com/c502137733.js"></script>
+//     </head>
+//     ​
+//     <body>
+//         <div class="container-fluid">
+//             <div class="row">
+//                 <div class="col-12 jumbotron mb-3 team-heading">
+//                     <h1 class="text-center">My Team</h1>
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="container">
+//             <div class="row">
+//                 <div class="team-area col-12 d-flex justify-content-center">
+//                     ${generateTeam(team)}
+//                 </div>
+//             </div>
+//         </div>
+//     </body>
+//     </html>
+//         `;
+//     };
+
+   
